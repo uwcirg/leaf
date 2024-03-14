f9478a9ee5c0f7e51757250a5071222728b0d5b3
Ivan Cvitkovic <ivanc@uw.edu>
2024-03-13 17:27:42 -0700

    Add docker entrypoint script for MSSQL

---
+++ src/db/build/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -eu
+
+repo_path="$(cd "$(dirname "$0")" && pwd)"
+cmdname="$(basename "$0")"
+
+usage() {
+    cat << USAGE >&2
+Usage:
+    $cmdname command
+
+    Docker entrypoint script
+    Wrapper script that executes docker-related tasks before running given command
+
+USAGE
+    exit 1
+}
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    usage
+    exit 0
+fi
+
+
+
+echo $cmdname complete
+echo executing given command $@
+exec "$@"
