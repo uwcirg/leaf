870c90a2b822922b2d36b82f57f609db96c66eb3
Amy Chen <clone@unbiunium.cirg.washington.edu>
2024-12-31 10:39:38 -0800

    add frontend piwik tracking code

---
+++ src/ui-client/src/utils/piwik.ts
@@ -0,0 +1,52 @@
+import {getEnvData} from "./envConfig";
+
+// get user Id from config, i.e. see getAuthConfig, getUserTokenAndContext from authAPI
+export function getUserIdFromEnv(config) : number | string | null {
+    if (!config) return null;
+    if (config.user && config.user.name) return config.user.name;
+    if (config.profile) return config.profile;
+    if (config.fhirUser) return config.fhirUser;
+    if (config["preferred_username"]) return config["preferred_username"];
+    return null;
+}
+// get PIWIK siteId from environment variable
+export function getMatomoSiteIdFromEnv(config) : number | null{
+    if (!config) return null;
+    if (config["REACT_APP_MATOMO_SITE_ID"]) return config["REACT_APP_MATOMO_SITE_ID"];
+    if (config["MATOMO_SITE_ID"]) return config["MATOMO_SITE_ID"];
+    if (config["REACT_APP_PIWIK_SITE_ID"]) return config["REACT_APP_PIWIK_SITE_ID"];
+    if (config["PIWIK_SITE_ID"]) return config["PIWIK_SITE_ID"];
+    return config["SITE_ID"];
+}
+export async function addMatomoTracking() {
+    // already generated script, return
+    if (!window || document.querySelector("#matomoScript")) return;
+    const envData = await getEnvData();
+    const userId = getUserIdFromEnv(envData);
+    console.log("PIWIK userId ", userId);
+    // no user Id return
+    if (!userId) return;
+    const siteId = getMatomoSiteIdFromEnv(envData);
+    console.log("PIWIK siteId ", siteId)
+    // no site Id return
+    if (!siteId) return;
+    // init global piwik tracking object
+    window._paq = [];
+    window._paq.push(["trackPageView"]);
+    window._paq.push(["enableLinkTracking"]);
+    window._paq.push(["setSiteId", siteId]);
+    window._paq.push(["setUserId", userId]);
+  
+    let u = "https://piwik.cirg.washington.edu/";
+    window._paq.push(["setTrackerUrl", u + "matomo.php"]);
+    let d = document,
+      g = d.createElement("script"),
+      headElement = document.querySelector("head");
+    g.type = "text/javascript";
+    g.async = true;
+    g.defer = true;
+    g.setAttribute("src", u + "matomo.js");
+    g.setAttribute("id", "matomoScript");
+    headElement.appendChild(g);
+  }
+  
